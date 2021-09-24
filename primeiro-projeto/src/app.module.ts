7a4c5bee6ed285c5068e2e21bfaea4084b217628
BrunoTruber <brunotruber45@gmail.com>
2021-09-24 19:16:05 -0300

    new

--- primeiro-projeto/src/app.module.ts
+++ primeiro-projeto/src/app.module.ts
@@ -1,7 +1,9 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FilmesModule } from './filmes/filmes.module';
+
 
 @Module({
   imports: [FilmesModule],
