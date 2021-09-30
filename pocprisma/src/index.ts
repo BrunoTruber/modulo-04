e7eb27c441d5f5cb0e4c9be5ae889e2e613a55d9
BrunoTruber <brunotruber45@gmail.com>
2021-09-29 23:05:29 -0300

    new

--- pocprisma/src/index.ts
+++ pocprisma/src/index.ts
@@ -3,27 +3,28 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-   await prisma.usuario.create({
-         data: {
-            id: 'user-3',
-            name: 'John3',
-            email: 'teste',
-            groups: {
-              connectOrCreate:[
-                  {
-                  where: {
-                      id: 'group3'
-                  },
-                  create: {
-                      id: 'group3',
-                      title: 'testete'
-                },
-                // se eu quiser adicionar mais grupos
-               },
-            ]
-          }
-        }
-    })
+  await prisma.usuario.create({
+    data: {
+      id: "user-0",
+      name: "Bruno",
+      email: "bruno@prisma",
+      groups: {
+        connectOrCreate: [
+          {
+            where: {
+              id: 'group3'
+            },
+            create: {
+              id: 'group3',
+              title: 'nao gostei de prisma'
+            },
+            // se eu quiser adicionar mais grupos
+          },//inicia o objeto na linha abaixo
+        ]
+      }
+    }
+  })
 }
+
 
 main();
